b95b5981c980a3de2e34191d9f66dde9aa768703
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-04-24 17:38:19 -0400

    added about me and nav components

---
+++ photo-port/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
